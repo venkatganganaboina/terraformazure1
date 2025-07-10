c56e26964d289ea33538521bdeb293a5c5a0cf39
Venkat <venkatganganaboina@gmail.com>
2025-07-10 16:05:48 +0530

    tags

--- rg.tf
+++ rg.tf
@@ -1,6 +1,10 @@
 resource "azurerm_resource_group" "rg" {
   name = "venkat"
   location = "Central India"
+
+  tags = {
+    user ="venkat"
+  }
 }
 resource "azurerm_resource_group" "rg1" {
   name = "venkat1"
